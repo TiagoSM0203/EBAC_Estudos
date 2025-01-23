00a43d62cebd8d4a6ffaa385bf9f3ba9ebfbe2bb
Tiago <tiago@teste.com>
2025-01-23 06:07:29 -0300

    att

---
+++ Typescript/src/funcoes.ts
@@ -0,0 +1,10 @@
+function calculaArea(base: number, altura: number) {
+    return base * altura
+}
+
+const calculaArea2 = (base: number, altura: number): number => base * altura
+
+function somar(...numeros: number[]) {
+    // numeros.reduce()
+    console.log(numeros)
+}
